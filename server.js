e0c00bbeb90e8a10bd33f48b3c4cc7b55ea48306
NardaPrin <nardaarandria@gmail.com>
2024-03-08 15:56:05 +0300

    modification URI de connexion Mongo DB

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ mongoose.Promise = global.Promise;
 // mongoose.set('debug', true);
 
 // remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud s
-const uri = 'mongodb+srv://mb1:toto@cluster0.lxvcyxy.mongodb.net/assignments?retryWrites=true&w=majority&appName=Cluster0';
+//const uri = 'mongodb+srv://mb1:toto@cluster0.lxvcyxy.mongodb.net/assignments?retryWrites=true&w=majority&appName=Cluster0';
+const uri = 'mongodb+srv://assigmentapp:assigmentapp123@cluster0.slrjbpl.mongodb.net/assignments?retryWrites=true&w=majority&appName=Cluster0';
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
